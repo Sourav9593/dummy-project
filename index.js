12f3c52b1773f7558cef2c78cce082df25f24535
Sourav9593 <sd959361@gmail.com>
2024-05-27 10:48:24 +0530

    commit3

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const input = document.getElementById("input")
 const menu = document.getElementById("menu")
 const sideMenu = document.getElementById("side-menu")
 
-search.addEventListener("click" ,()=>{
-    if(search.classList.contains("fa-magnifying-glass")){
+search.addEventListener("click", () => {
+    if (search.classList.contains("fa-magnifying-glass")) {
         search.classList.remove("fa-magnifying-glass");
         search.classList.add("fa-xmark");
-        input.style.display="block";
+        input.style.display = "block";
     }
-    else if(search.classList.contains("fa-xmark")){
+    else if (search.classList.contains("fa-xmark")) {
         search.classList.remove("fa-xmark");
         search.classList.add("fa-magnifying-glass");
-        input.style.display="none";
+        input.style.display = "none";
     }
 })
 
-menu.addEventListener("click",()=>{
-    if(menu.classList.contains("fa-bars")){
+menu.addEventListener("click", () => {
+    if (menu.classList.contains("fa-bars")) {
         menu.classList.remove("fa-bars");
         menu.classList.add("fa-xmark");
-        sideMenu.style.display="block";
+        sideMenu.style.display = "block";
     }
-    else if(menu.classList.contains("fa-xmark")){
+    else if (menu.classList.contains("fa-xmark")) {
         menu.classList.remove("fa-xmark");
         menu.classList.add("fa-bars");
-        sideMenu.style.display="none";
+        sideMenu.style.display = "none";
     }
 })
